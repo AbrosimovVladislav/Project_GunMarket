4f6d9f5c559f07b6d4c3542d98ac775c18a8b623
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2018-11-25 11:16:00 +0300

    GUNMARKET-6 Начало работы над запросом из таблицы shops

--- src/main/java/com/gunmarket/web/ShopController.java
+++ src/main/java/com/gunmarket/web/ShopController.java
@@ -5,9 +5,13 @@ import com.gunmarket.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ShopController {
@@ -15,7 +19,21 @@ public class ShopController {
     @Autowired
     ShopService shopService;
 
-    @RequestMapping(value = "/shops", method = RequestMethod.GET, headers = "Accept=application/json")
+/*    @RequestMapping(value = "/shopsByParams", method = RequestMethod.GET, headers = "Accept=application/json")
+    public List<Shop> getShopsByParams(@RequestParam(value = PRICE_NAME, required = false) String price
+            , @RequestParam(value = TYPEID_NAME, required = false) String typeId) {
+        Map<String,List<String>> params = new HashMap<String,List<String>>();
+        if(price!=null){
+            params.put(PRICE_NAME, Arrays.asList(price.split(",")));
+        }
+        if(typeId!=null){
+            params.put(TYPEID_NAME, Arrays.asList(typeId.split(",")));
+        }
+
+        return productService.getProductsByParams(params);
+    }*/
+
+    @RequestMapping(value = "/allShops", method = RequestMethod.GET, headers = "Accept=application/json")
     public List<Shop> getShops() {
         return shopService.getAllShops();
     }
