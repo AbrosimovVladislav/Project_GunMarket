277d1588c1f1ea30af01c47a5413fce7d5c52430
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2018-12-01 00:29:53 +0500

    GUNMARKET-6 Добавление в контроллерный слой логики обработки запросов с простыми параметрами

--- src/main/java/com/gunmarket/web/ProductController.java
+++ src/main/java/com/gunmarket/web/ProductController.java
@@ -8,35 +8,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.gunmarket.model.Product.PRODUCT_PRICE;
+import static com.gunmarket.model.Type.TYPE_ID;
 
 @RestController
 public class ProductController {
 
-    private static final String PRICE_NAME = "price";
-    private static final String TYPEID_NAME = "type_id";
-
     @Autowired
     ProductService productService;
 
-        @RequestMapping(value = "/productsByParams", method = RequestMethod.GET, headers = "Accept=application/json")
-        public List<Product> getProductsByParams(@RequestParam(value = PRICE_NAME, required = false) String price
-                , @RequestParam(value = TYPEID_NAME, required = false) String typeId) {
-            Map<String,List<String>> params = new HashMap<String,List<String>>();
-            if(price!=null){
-                params.put(PRICE_NAME, Arrays.asList(price.split(",")));
-            }
-            if(typeId!=null){
-                params.put(TYPEID_NAME, Arrays.asList(typeId.split(",")));
-            }
-
-            return productService.getProductsByParams(params);
+    @RequestMapping(value = "/products", method = RequestMethod.GET, headers = "Accept=application/json")
+    public List<Product> getProductsByParams(@RequestParam(value = PRODUCT_PRICE, required = false) String price
+            , @RequestParam(value = TYPE_ID, required = false) String typeId) {
+        Map<String, List<String>> params = new HashMap<String, List<String>>();
+        if (price != null) {
+            params.put(PRODUCT_PRICE, Arrays.asList(price.split(",")));
+        }
+        if (typeId != null) {
+            params.put(TYPE_ID, Arrays.asList(typeId.split(",")));
         }
 
-    @RequestMapping(value = "/allProducts", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<Product> getProducts() {
-        return productService.getAllProducts();
+        return productService.getProductsByParams(params);
     }
 
 }
