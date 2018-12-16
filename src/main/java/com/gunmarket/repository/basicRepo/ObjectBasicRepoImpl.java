3f10f9e06152456d14f460524ee250c7d16917c6
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2018-12-16 15:04:04 +0300

    GUNMARKET-25 Упразднение системы наполнения через статический счетчик, путем введения нового класса ParameterValue, содержащего в себе как значения перередаваемых параметров так и маркер заполнения для каждого из значений

--- src/main/java/com/gunmarket/repository/basicRepo/ObjectBasicRepoImpl.java
+++ src/main/java/com/gunmarket/repository/basicRepo/ObjectBasicRepoImpl.java
@@ -2,6 +2,7 @@ package com.gunmarket.repository.basicRepo;
 
 import com.gunmarket.repository.basicRepo.queryBuilder.QueryBuilder;
 import com.gunmarket.web.HttpParameter;
+import com.gunmarket.web.ParameterValue;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Map;
 
+import static com.gunmarket.repository.basicRepo.repoUtils.RepoUtils.addMarkersToParams;
 import static com.gunmarket.repository.basicRepo.repoUtils.RepoUtils.sortParamsMap;
 
 @Repository
@@ -19,8 +21,6 @@ public class ObjectBasicRepoImpl implements ObjectBasicRepo {
     @Autowired
     private SessionFactory sessionFactory;
 
-    private static int paramRepoCounter = 0;
-
     private QueryBuilder queryBuilder = new QueryBuilder();
 
     private Session currentSession() {
@@ -28,13 +28,14 @@ public class ObjectBasicRepoImpl implements ObjectBasicRepo {
     }
 
     public List getByParamsDueHql(String entityName, Map<HttpParameter, List<String>> params) {
-        Query query = currentSession().createQuery(queryBuilder.build(entityName, params));
-        for (Map.Entry<HttpParameter, List<String>> entry : sortParamsMap(params).entrySet()) {
-            for (String value : entry.getValue()) {
+        Map<HttpParameter, List<ParameterValue>> markedparams = addMarkersToParams(params);
+
+        Query query = currentSession().createQuery(queryBuilder.build(entityName, markedparams));
+        for (Map.Entry<HttpParameter, List<ParameterValue>> entry : sortParamsMap(markedparams).entrySet()) {
+            for (ParameterValue parameterValue : entry.getValue()) {
                 String paramClass = entry.getKey().getParamClass();
-                query.setParameter("p" + value + paramRepoCounter + "n",
-                        paramClass.equals("Long") ? Long.valueOf(value) : value);
-                paramRepoCounter++;
+                query.setParameter(parameterValue.getValueMarker().replaceFirst(":", ""),
+                        paramClass.equals("Long") ? Long.valueOf(parameterValue.getValue()) : parameterValue.getValue());
             }
         }
 
