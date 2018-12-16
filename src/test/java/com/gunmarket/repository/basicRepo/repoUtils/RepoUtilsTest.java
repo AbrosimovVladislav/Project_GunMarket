5f1eb3ccf61b649fd7841e4cda9f24bebf8ee858
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2018-12-16 20:26:56 +0300

    GUNMARKET-9 Загрузка пустых классов для будущих тестов

--- src/test/java/com/gunmarket/repository/basicRepo/repoUtils/RepoUtilsTest.java
+++ src/test/java/com/gunmarket/repository/basicRepo/repoUtils/RepoUtilsTest.java
@@ -1,12 +1,6 @@
 package com.gunmarket.repository.basicRepo.repoUtils;
 
 public class RepoUtilsTest {
-    private static RepoUtilsTest ourInstance = new RepoUtilsTest();
 
-    public static RepoUtilsTest getInstance() {
-        return ourInstance;
-    }
 
-    private RepoUtilsTest() {
-    }
 }
