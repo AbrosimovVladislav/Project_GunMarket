7447c0772b5f06d15c01375346af779e02d25075
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2019-06-16 20:27:23 +0300

    change absolute path ro relative path

--- src/main/java/pro/va/gunmarket/service/MenuService.java
+++ src/main/java/pro/va/gunmarket/service/MenuService.java
@@ -1,23 +1,18 @@
 package pro.va.gunmarket.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 @Service
 public class MenuService {
-
-	private static final Path menuPath = Paths.get(
-			"C:\\Vlad\\ProgFolder\\IdeaProjects\\GunMarketSpare\\src\\main\\resources\\menu.json"
-	);
+	private static final String menuLocation = "classpath:menu.json";
 
 	public String getMenu() {
 		try {
-			return Files.lines(menuPath).collect(Collectors.joining());
+			return new String(Files.readAllBytes(ResourceUtils.getFile(menuLocation).toPath()));
 		} catch (IOException e) {
 			return e.toString();
 		}
