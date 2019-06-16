e3168085f877a3fd0c60c4bb8eda41f20a99572f
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2019-06-16 20:16:24 +0300

    StartApp

--- src/main/java/pro/va/gunmarket/StartApp.java
+++ src/main/java/pro/va/gunmarket/StartApp.java
@@ -3,11 +3,9 @@ package pro.va.gunmarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "pro.va.gunmarket.*")
+@SpringBootApplication
 public class StartApp {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class);
 	}
-
 }
