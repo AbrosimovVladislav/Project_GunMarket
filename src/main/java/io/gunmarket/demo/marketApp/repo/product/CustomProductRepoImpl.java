2298b9cd1714fcd0a8fd197256a2f1ba94451983
AbrosimovVladislav <32846897+AbrosimovVladislav@users.noreply.github.com>
2019-12-25 00:17:29 +0300

    Gm#43 (#45)
    * GM#43: pagination added
    
    * GM #43 : pagination added

--- src/main/java/io/gunmarket/demo/marketApp/repo/product/CustomProductRepoImpl.java
+++ src/main/java/io/gunmarket/demo/marketApp/repo/product/CustomProductRepoImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
@@ -18,16 +19,25 @@ import java.util.Map;
 @Repository
 @RequiredArgsConstructor
 public class CustomProductRepoImpl implements CustomProductRepo {
-	@PersistenceContext private final EntityManager entityManager;
-	private final QueryBuilder queryBuilder;
+    @PersistenceContext
+    private final EntityManager entityManager;
+    private final QueryBuilder queryBuilder;
 
-	@Override
-	public List<Product> findAllByParameters(Map<String, String> requestParams, Pageable pageable) {
-		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Product> criteriaQuery =
-				queryBuilder.createCriteriaQueryFromParamMap(criteriaBuilder, requestParams, Product.class);
-		queryBuilder.addSort(criteriaBuilder, criteriaQuery, pageable);
-		return entityManager.createQuery(criteriaQuery).getResultList();
-	}
+    @Override
+    public List<Product> findAllByParameters(Map<String, String> requestParams, Pageable pageable) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery =
+                queryBuilder.createCriteriaQueryFromParamMap(criteriaBuilder, requestParams, Product.class);
+        queryBuilder.addSort(criteriaBuilder, criteriaQuery, pageable);
+        TypedQuery<Product> productQuery = entityManager.createQuery(criteriaQuery);
+        productQuery = addPagination(productQuery, pageable);
+        return productQuery.getResultList();
+    }
+
+    private TypedQuery<Product> addPagination(TypedQuery<Product> productQuery, Pageable pageable) {
+        productQuery.setFirstResult(pageable.getPageSize() * pageable.getPageNumber());
+        productQuery.setMaxResults(pageable.getPageSize());
+        return productQuery;
+    }
 
 }
--- src/main/java/io/gunmarket/demo/marketApp/web/controller/ProductController.java
+++ src/main/java/io/gunmarket/demo/marketApp/web/controller/ProductController.java
@@ -2,11 +2,13 @@ package io.gunmarket.demo.marketApp.web.controller;
 
 import io.gunmarket.demo.marketApp.domain.Product;
 import io.gunmarket.demo.marketApp.service.ProductService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.internal.StringUtil;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.util.Pair;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,25 +22,55 @@ import static io.gunmarket.demo.marketApp.domain.Rating.RATING_VALUE_SORT;
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
-	private final ProductService productService;
+    private final ProductService productService;
 
-	@GetMapping(value = "/products", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<Product> getAllByParams(@RequestParam Map<String, String> requestParams, Pageable pageable) {
-		Pair<Map<String, String>, Pageable> pairOfParamsAndPageable = validate(requestParams, pageable);
-		return productService.getAllByParameters(pairOfParamsAndPageable.getFirst(),
-				pairOfParamsAndPageable.getSecond());
-	}
+    private static final int DEFAULT_PAGE_NUMBER = 0;
+    private static final int DEFAULT_PAGE_SIZE = 10;
 
-	private Pair<Map<String, String>, Pageable> validate(Map<String, String> requestParams, Pageable pageable) {
-		String sortParam = requestParams.remove("sort");
-		if (sortParam == null) return Pair.of(requestParams, getDefaultSortingProperty(pageable));
-		String sortProp = sortParam.substring(0, sortParam.indexOf(","));
-		Sort sort = Sort.by(pageable.getSort().getOrderFor(sortProp));
-		PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
-		return Pair.of(requestParams, pageRequest);
-	}
+    @GetMapping(value = "/products", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllByParams(@RequestParam Map<String, String> requestParams,
+                                        @PageableDefault(size = DEFAULT_PAGE_SIZE, page = DEFAULT_PAGE_NUMBER) Pageable pageable) {
+        FilterAndPageable pairOfParamsAndPageable = validate(requestParams, pageable);
+        return productService.getAllByParameters(pairOfParamsAndPageable.filter, pairOfParamsAndPageable.pageable);
+    }
 
-	private static PageRequest getDefaultSortingProperty(Pageable pageable) {
-		return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(RATING_VALUE_SORT));
-	}
+    private FilterAndPageable validate(Map<String, String> requestParams, Pageable pageable) {
+        FilterAndPageable filterAndPageable = new FilterAndPageable(requestParams, pageable);
+        sortingValidation(filterAndPageable);
+        paginationValidation(filterAndPageable);
+        return filterAndPageable;
+    }
+
+    private void sortingValidation(FilterAndPageable filterAndPageable) {
+        Pageable pageable = filterAndPageable.pageable;
+        String sortParam = filterAndPageable.filter.remove("sort");
+        if (sortParam == null) {
+            filterAndPageable.pageable = getDefaultSortingProperty(pageable);
+        } else {
+            String sortProp = sortParam.substring(0, sortParam.indexOf(","));
+            Sort sort = Sort.by(pageable.getSort().getOrderFor(sortProp));
+            filterAndPageable.pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
+        }
+    }
+
+    private void paginationValidation(FilterAndPageable filterAndPageable) {
+        Pageable pageable = filterAndPageable.pageable;
+        String page = filterAndPageable.filter.remove("page");
+        String size = filterAndPageable.filter.remove("size");
+
+        if (StringUtil.isNumeric(page))
+            filterAndPageable.pageable = PageRequest.of(Integer.parseInt(page), pageable.getPageSize(), pageable.getSort());
+        if (StringUtil.isNumeric(size))
+            filterAndPageable.pageable = PageRequest.of(pageable.getPageNumber(), Integer.parseInt(size), pageable.getSort());
+    }
+
+    private static PageRequest getDefaultSortingProperty(Pageable pageable) {
+        return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(RATING_VALUE_SORT));
+    }
+
+    @AllArgsConstructor
+    private static class FilterAndPageable {
+        Map<String, String> filter;
+        Pageable pageable;
+    }
 }
