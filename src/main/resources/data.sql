f6dabd8bea253672c7619a8879125552991c0c9e
AbrosimovVladislav <32846897+AbrosimovVladislav@users.noreply.github.com>
2019-12-28 22:13:55 +0300

    #48 : pis controller have new endpoint productsInShops/{productId} (#51)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -54,6 +54,7 @@ INSERT INTO product_in_shop (product_in_shop_id, additional_info, in_stock, link
 INSERT INTO product_in_shop (product_in_shop_id, additional_info, in_stock, link, price, sale, product_id, shop_id, popularity) VALUES ('8:2','info', true,  'link', 10, 0, 8,  2, 0.8);
 INSERT INTO product_in_shop (product_in_shop_id, additional_info, in_stock, link, price, sale, product_id, shop_id, popularity) VALUES ('9:2','info', true,  'link', 10, 0, 9,  2, 0.9);
 INSERT INTO product_in_shop (product_in_shop_id, additional_info, in_stock, link, price, sale, product_id, shop_id, popularity) VALUES ('10:2','info', false, 'link', 10, 5, 10, 2, 0.99);
+INSERT INTO product_in_shop (product_in_shop_id, additional_info, in_stock, link, price, sale, product_id, shop_id, popularity) VALUES ('1:2','info', false, 'link', 10, 5, 1, 2, 0.99);
 
 INSERT INTO rating (value, product_id) VALUES ('J',1);
 INSERT INTO rating (value, product_id) VALUES ('I',2);
