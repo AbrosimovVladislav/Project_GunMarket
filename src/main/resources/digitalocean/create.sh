77ab75f48e3a6b5461a4ca12ef3f6b63d1f3492b
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-04-29 00:01:07 +0300

    develop: digital ocean fixes

--- src/main/resources/digitalocean/create.sh
+++ src/main/resources/digitalocean/create.sh
@@ -1,6 +1,9 @@
 sudo apt-get update
+
+# docker
 sudo apt install docker.io
-sudo apt install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # front
 git clone https://github.com/AbrosimovVladislav/JackNorthon
