73f61586ac99f1397b9b1e3aa1507e23c2ff0081
sildvauresberg <slickunit4265752@gmail.com>
2020-05-05 23:31:07 +0300

    Update create.sh

--- src/main/resources/digitalocean/create.sh
+++ src/main/resources/digitalocean/create.sh
@@ -32,4 +32,4 @@ sudo apt -y install docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 cd JackNorthon/
-docker-compose up
+docker-compose up --build -d
